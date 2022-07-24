c4400efb01957c88724023ad261fe8897361844f
smeshchankin <smeshchankin@gmail.com>
2022-07-24 19:09:34 +0300

    Init database

---
+++ src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,3 @@
+insert into todo values ('7fd921cfd2b64dc7b995633e8209f385', 'Buy Milk', '2022-07-23 15:00:01', '2022-07-23 15:00:01', false);
+insert into todo values ('5820a4c2abe74f409da89217bf905a0c', 'Read a book', '2022-07-02 16:00:01', '2022-07-02 16:00:01', false);
+insert into todo values ('a44b6db26aef49e39d1b68622f55c347', 'Go to Spring One 2022', '2022-07-18 12:00:00', '2022-07-18 12:00:00', false);
